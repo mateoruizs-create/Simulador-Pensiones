2f659e13fd9c18ed959b8c7a61931087fcad6a95
mateoruizs-create <mateoruizs@gmail.com>
2025-09-24 22:44:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
 const CACHE_NAME = 'simulador-pensiones-v1';
+const BASE_PATH = '/Simulador-Pensiones';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  BASE_PATH + '/',
+  BASE_PATH + '/index.html',
+  BASE_PATH + '/manifest.json',
   'https://cdn.tailwindcss.com',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
